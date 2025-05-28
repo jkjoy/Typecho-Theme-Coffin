6989017e13bb2d965466141821be6090e129075d
浪子 <jkjoy@live.cn>
2025-05-28 09:23:10 +0800

    0.2.1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * 一款单栏主题. Coffin 2.0 原作者 bigfa  
  * @package Coffin 2.0
  * @author  老孙 
- * @version 0.2.0
+ * @version 0.2.1
  * @link https://www.imsun.org
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
@@ -18,7 +18,7 @@ if ($sticky && !empty(trim($sticky))) {
     if (!empty($sticky_cids)) {
         $sticky_html = " <span class='sticky--post'><svg xmlns='http://www.w3.org/2000/svg' width='16px' height='16px' fill='none' viewBox='0 0 24 24' class='bk'>
 <path fill='#242424' fill-rule='evenodd' d='M12.333 16.993a7.4 7.4 0 0 1-1.686-.12 7.25 7.25 0 1 1 8.047-4.334v.001a7.2 7.2 0 0 1-.632 1.188 7.26 7.26 0 0 1-4.708 3.146l-.07.013q-.466.083-.951.105m.356.979a8.4 8.4 0 0 1-1.377 0l-2.075 5.7a.375.375 0 0 1-.625.13l-2.465-2.604-3.563.41a.375.375 0 0 1-.395-.501l2.645-7.267a8.25 8.25 0 1 1 14.333 0l2.645 7.267a.375.375 0 0 1-.396.5l-3.562-.41-2.465 2.604a.375.375 0 0 1-.625-.13zm5.786-3.109a8.25 8.25 0 0 1-4.775 2.962l1.658 4.554 1.77-1.87.344-.362.496.057 2.558.294zm-12.95 0L3.476 20.5l2.557-.295.497-.057.344.363 1.77 1.87 1.658-4.555a8.25 8.25 0 0 1-4.775-2.961' clip-rule='evenodd'></path>
-</svg>「置顶」 </span> ";
+</svg></span> ";
         
         // 保存原始对象状态
         $originalRows = $this->row;
