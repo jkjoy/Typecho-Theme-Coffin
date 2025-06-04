1c8613bac5b70060dc0a71dec1eb4a9907ea59d7
浪子 <jkjoy@live.cn>
2025-06-04 19:41:36 +0800

    0.2.2
    刷新版本号

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * 一款单栏主题. Coffin 2.0 原作者 bigfa  
  * @package Coffin 2.0
  * @author  老孙 
- * @version 0.2.1
+ * @version 0.2.2
  * @link https://www.imsun.org
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
@@ -19,18 +19,15 @@ if ($sticky && !empty(trim($sticky))) {
         $sticky_html = " <span class='sticky--post'><svg xmlns='http://www.w3.org/2000/svg' width='16px' height='16px' fill='none' viewBox='0 0 24 24' class='bk'>
 <path fill='#242424' fill-rule='evenodd' d='M12.333 16.993a7.4 7.4 0 0 1-1.686-.12 7.25 7.25 0 1 1 8.047-4.334v.001a7.2 7.2 0 0 1-.632 1.188 7.26 7.26 0 0 1-4.708 3.146l-.07.013q-.466.083-.951.105m.356.979a8.4 8.4 0 0 1-1.377 0l-2.075 5.7a.375.375 0 0 1-.625.13l-2.465-2.604-3.563.41a.375.375 0 0 1-.395-.501l2.645-7.267a8.25 8.25 0 1 1 14.333 0l2.645 7.267a.375.375 0 0 1-.396.5l-3.562-.41-2.465 2.604a.375.375 0 0 1-.625-.13zm5.786-3.109a8.25 8.25 0 0 1-4.775 2.962l1.658 4.554 1.77-1.87.344-.362.496.057 2.558.294zm-12.95 0L3.476 20.5l2.557-.295.497-.057.344.363 1.77 1.87 1.658-4.555a8.25 8.25 0 0 1-4.775-2.961' clip-rule='evenodd'></path>
 </svg></span> ";
-        
         // 保存原始对象状态
         $originalRows = $this->row;
         $originalStack = $this->stack;
         $originalLength = $this->length;
         $totalOriginal = $this->getTotal();
-        
         // 重置当前对象状态
         $this->row = [];
         $this->stack = [];
         $this->length = 0;
-  
         if (isset($this->currentPage) && $this->currentPage == 1) {
             // 查询置顶文章
             $selectSticky = $this->select()->where('type = ?', 'post');
@@ -41,7 +38,6 @@ if ($sticky && !empty(trim($sticky))) {
                     $selectSticky->orWhere('cid = ?', $cid);
             }
             $stickyPosts = $db->fetchAll($selectSticky);
-            
             // 添加置顶文章到结果集
             foreach ($stickyPosts as &$stickyPost) {
                 $stickyPost['isSticky'] = true;
@@ -65,32 +61,23 @@ if ($sticky && !empty(trim($sticky))) {
                 ->where('type = ?', 'post')
                 ->where('status = ?', 'publish')
                 ->where('created < ?', time());
-                
             // 排除所有置顶文章
             foreach ($sticky_cids as $cid) {
                 $selectNormal->where('table.contents.cid != ?', $cid);
             }
-            
             $selectNormal->order('created', Typecho_Db::SORT_DESC)
                 ->limit($standardPageSize)
                 ->offset(0);
         } else {
-            // 非第一页的查询
-            // 计算正确的偏移量：(当前页码-1) * 每页数量 - 置顶文章数
-            // 这样可以确保不会漏掉文章
             $offset = ($this->currentPage - 1) * $pageSize - count($sticky_cids);
             $offset = max($offset, 0); // 确保偏移量不为负
-            
             $selectNormal = $this->select()
                 ->where('type = ?', 'post')
                 ->where('status = ?', 'publish')
                 ->where('created < ?', time());
-                
-            // 排除所有置顶文章
             foreach ($sticky_cids as $cid) {
                 $selectNormal->where('table.contents.cid != ?', $cid);
             }
-            
             $selectNormal->order('created', Typecho_Db::SORT_DESC)
                 ->limit($pageSize)
                 ->offset($offset);
