21bb42dfedb7bb9e61e4ccb16e3657b1062c2346
浪子 <jkjoy@live.cn>
2025-12-01 18:57:11 +0800

    fix bug
    捉到一只小虫子

--- post.php
+++ post.php
@@ -13,7 +13,7 @@
                 <a href="<?php $this->options->adminUrl('write-post.php?cid=' . $this->cid); ?>" target="_blank" title="编辑文章">Edit</a>
                 <?php endif; ?>              
             </div>
-                <h2 class="entry--title" itemprop="headline"><?php $this->title() ?></h2>            
+            <h2 class="entry--title" itemprop="headline"><?php $this->title() ?></h2>            
         </header>
             <meta itemscope itemprop="mainEntityOfPage" itemType="https://schema.org/WebPage" itemid="<?php $this->permalink() ?>" />
             <meta itemprop="datePublished" content="<?php $this->date('Y-m-d'); ?>" />
@@ -31,7 +31,7 @@
 <path d="M6.5 7.5a1 1 0 1 0 2 0a1 1 0 1 0-2 0" />
 <path d="M3 6v5.172a2 2 0 0 0 .586 1.414l7.71 7.71a2.41 2.41 0 0 0 3.408 0l5.592-5.592a2.41 2.41 0 0 0 0-3.408l-7.71-7.71A2 2 0 0 0 11.172 3H6a3 3 0 0 0-3 3" />
 </g>
-</svg>>
+</svg>
 <?php echo $tag['name']; ?>
 </a> 
 <?php endforeach; ?>
